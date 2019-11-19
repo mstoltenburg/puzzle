11b14c0fdae23dff5f79bf3605efa834810832de
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2019-11-20 00:53:46 +0100

    Hide preview

--- src/components/puzzle/Preview.jsx
+++ src/components/puzzle/Preview.jsx
@@ -1,16 +1,24 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import imageSrc from 'app/images/IMG_0356.jpeg';
 
 const Preview = forwardRef(({ format, updatePuzzle }, ref) => {
+    const [hidden, setHidden] = useState(true);
+    const toggle = () => setHidden(!hidden);
     const updateImage = ({ target }) => {
         updatePuzzle({ type: 'image', image: target });
     };
 
     return (
-        <div className={`preview preview--${format}`}>
-            <img className="preview__image" src={imageSrc} ref={ref} onLoad={updateImage} alt="Vorschau" />
+        <div className="preview">
+            <div className={`preview__canvas preview__canvas--${format}`} hidden={hidden}>
+                <img className="preview__image" src={imageSrc} ref={ref} onLoad={updateImage} alt="Vorschau" />
+            </div>
+            <label className="preview__label" htmlFor="preview">
+                <input id="preview" type="checkbox" checked={!hidden} onChange={toggle} />
+                Vorschau anzeigen
+            </label>
         </div>
     );
 });
